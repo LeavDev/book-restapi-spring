e48c4664f0fb83357ad125f7bfc8526b39b270df
LeavDev <learning13062002@gmail.com>
2025-05-10 08:20:58 +0700

    test author create endpoint

--- src/main/java/com/dwi/restapi/controllers/AuthorController.java
+++ src/main/java/com/dwi/restapi/controllers/AuthorController.java
@@ -1,5 +1,7 @@
 package com.dwi.restapi.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,9 +24,9 @@ public class AuthorController {
     }
 
     @PostMapping(path = "/authors")
-    public AuthorDto createAuthor(@RequestBody AuthorDto author) {
+    public ResponseEntity<AuthorDto> createAuthor(@RequestBody AuthorDto author) {
         AuthorEntity authorEntity = authorMapper.mapFrom(author);
         AuthorEntity savedAuthorEntity = authorService.createAuthor(authorEntity);
-        return authorMapper.mapTo(savedAuthorEntity);
+        return new ResponseEntity<>(authorMapper.mapTo(savedAuthorEntity), HttpStatus.CREATED);
     }
 }
